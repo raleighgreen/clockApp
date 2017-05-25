263dc4652548ceff27dafcbdb7ad7aad1c226177
Raleigh Green <raleighgreen@gmail.com>
2017-05-25 17:39:38 -0400

    acc updateClock() func

--- clockApp/ViewController.swift
+++ clockApp/ViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // print("View has loaded")
+        myLabel.text = ""
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+        
     }
 
+    func updateClock() {
+        
+        //print("Updated from clock")
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
